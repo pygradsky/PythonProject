c18eda43b714503c24c8fce9f0623ab018125559
Fedor <feda.vert@gmail.com>
2026-04-23 20:00:37 +0300

    add tasks for each data structure

---
+++ Tasks/stacks/30_browser_history.py
@@ -0,0 +1,30 @@
+﻿"""
+1. Browser History
+
+2. РЈСЂРѕРІРµРЅСЊ
+hard
+
+3. РЈСЃР»РѕРІРёРµ
+Р РµР°Р»РёР·СѓР№С‚Рµ СЃС‚СЂСѓРєС‚СѓСЂСѓ `BrowserHistory` СЃ РѕРїРµСЂР°С†РёСЏРјРё `visit(url)`, `back(steps)` Рё `forward(steps)`. РСЃС‚РѕСЂРёСЏ РґРѕР»Р¶РЅР° РєРѕСЂСЂРµРєС‚РЅРѕ РѕР±СЂР°Р±Р°С‚С‹РІР°С‚СЊ РїРµСЂРµС…РѕРґС‹ РЅР°Р·Р°Рґ Рё РІРїРµСЂС‘Рґ, РєР°Рє РІ Р±СЂР°СѓР·РµСЂРµ.
+
+4. РџСЂРёРјРµСЂ(С‹)
+Р’С…РѕРґ:
+`BrowserHistory("leetcode.com")`
+`visit("google.com")`
+`visit("facebook.com")`
+`visit("youtube.com")`
+`back(1)`
+`back(1)`
+`forward(1)`
+Р’С‹С…РѕРґ:
+`"facebook.com"`
+`"google.com"`
+`"facebook.com"`
+
+Р’С…РѕРґ:
+`BrowserHistory("start.com")`
+`visit("a.com")`
+`back(1)`
+Р’С‹С…РѕРґ:
+`"start.com"`
+"""
